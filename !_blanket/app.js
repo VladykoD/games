cd4bfe17939f5af30b16a72f2947be7c02e6cff0
VladykoD <VladykoD@gmail.com>
2021-03-22 01:46:31 +0300

    added game loop and blank folder

---
+++ !_blanket/app.js
@@ -0,0 +1,11 @@
+import { Layer } from './layer.js';
+
+class App {
+    constructor(container) {
+        this.layer = new Layer(container);
+    }
+}
+
+onload = () => {
+    new App(document.querySelector('body'))
+}
